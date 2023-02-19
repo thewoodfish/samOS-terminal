b6a32e2cfdc29c7347f8e2fb92a7fcfbafb199a8
thewoodfish <jasonholt2002@gmail.com>
2023-02-19 16:40:13 +0100

    latest update

--- app.js
+++ app.js
@@ -1,26 +1,202 @@
 // imports
+import { createRequire } from "module";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { SamaritanSDK } from 'samaritan-js-sdk';
+import * as util from "./utility.js";
+import e from "express";
+
+const require = createRequire(import.meta.url);
 const express = require('express');
 const app = express();
 const port = 4000;
+const __filename = fileURLToPath(import.meta.url);
+const _Dirname = path.dirname(__filename);
 
 // static files
 app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + 'public/js'));
-app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/css', express.static(_Dirname + 'public/css'));
+app.use('/js', express.static(_Dirname + 'public/js'));
+app.use('/img', express.static(_Dirname + 'public/img'));
 
 // set views
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('', (req, res) => {
-    res.render('index', { text: 'This is sparta' });
+    res.render('terminal');
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', { text: 'Eragon and the dragon' });
+    res.render('about');
 });
+
+app.get('/new', (req, res) => {
+    createNewSamaritan(req.query, res);
+});
+
+app.get('/init', (req, res) => {
+    initializeSamaritan(req.query, res);
+});
+
+app.get('/desc', (req, res) => {
+    describeSamaritan(req.query, res);
+});
+
+app.get('/profile', (req, res) => {
+    manageProfile(req.query, res);
+});
+
+// initialize Samaritan SDK
+const sam = new SamaritanSDK("ws://127.0.0.1:1509");
+let terminal = undefined;
+await sam.init();
+
+// wait 5 seconds for initialization
+setTimeout(async () => {
+    terminal = await sam.did.auth("still plant flow death state perhaps percent study always taken information patient");
+}, 5000);
+
+// a very simple session cache
+class SessionCache {
+    cache = {}
+
+    get = (key) => {
+        return this.cache[key];
+    }
+
+    set = (key, value) => {
+        this.cache[key] = value;
+        return value;
+    }
+
+    del = (key) => {
+        const val = cache[key];
+        delete this.cache[key];
+        return val;
+    }
+
+    has = (key) => {
+        return key in this.cache;
+    }
+}
+
+let silverCache = new SessionCache();
+
+async function createNewSamaritan(req, res) {
+    // query network to create new Samaritan
+    const user = await sam.did.createNew(req.name);
+
+    // generate nonce
+    const nonce = util.randomStr(12);
+
+    // cache the user data
+    silverCache.set(nonce, {
+        did: user.did
+    });
+
+    // return details
+    res.send({
+        nonce,
+        data: user,
+        error: false
+    });
+}
+
+async function initializeSamaritan(req, res) {
+    // get the keys and query the network to check for existence of Samaritan
+    const auth = await sam.did.auth(req.keys);
+
+    if (auth.exists && auth.did.indexOf("sam:root") != -1) {
+        // set session
+        const nonce = util.randomStr(12);
+        silverCache.set(nonce, {
+            did: auth.did
+        });
+
+        res.send({
+            nonce,
+            data: {
+                msg: "samaritan initialization complete"
+            }
+        })
+        
+    } else {
+        res.send({
+            data: {
+                msg: "samaritan does not exist"
+            }
+        })
+    }
+}
+
+// describe a samaritan 
+async function describeSamaritan(req, res) {
+    const didDoc = await sam.did.describe(req.did);
+
+    if (didDoc) {
+        return res.send({
+            data: {
+                did: req.did,
+                name: didDoc.name
+            },
+            error: false
+        });
+    } else {
+        return res.send({
+            data: {
+                msg: "samaritan not recognised"
+            },
+            error: true
+        });
+    }
+}
+
+// check for nonce
+function authUser(nonce) {
+    if (silverCache.has(nonce)) {
+        return silverCache.get(nonce);
+    }
+
+    return false;
+}
+
+// get or set profile 
+async function manageProfile(req, res) {
+    // first check for auth
+    const user = authUser(req.nonce);
+    if (user) {
+        let profile = await sam.db.get(user.did, "$profile");
+        // check if it's a get or set
+        if (req.data) {
+            // if profile does not exist, set it
+            profile = profile ? profile : {};
+
+            // get profile first
+            profile = util.syncData(req.data.split(";"), profile);
+            // set data
+            await sam.db.insert(user.did, "$profile", profile);
+        }
+
+        return res.send({
+            data: {
+                profile: profile ? profile : {},
+                type: req.data ? "set" : "get"
+            },
+            error: false
+        });
+    } else {
+        return res.send({
+            data: {
+                msg: "samaritan not recognised"
+            },
+            error: true
+        });
+    }
+}
+
 
 
 // listen on port 3000
 app.listen(port, () => console.info(`listening on port ${port}`));
+
---
+++ public/js/cmd.js
@@ -0,0 +1,284 @@
+jQuery(function ($, undefined) {
+	var main = $('body').terminal({
+		sam: function (arg1 = "", arg2 = "", arg3 = "", arg4 = "", arg5 = "") {
+			switch (arg1) {
+				case "help":
+				case "":
+					this.echo(`samaritanOS v0.1.0`);
+					this.echo(`Usage: sam <command> [arg1] [arg2] [arg3] [argN]`);
+					this.echo(`These are common samaritan commands used in various situations:`);
+					this.echo(`new <name>			        creates a new samaritan with a non-unique name`);
+					this.echo(`init <keys>			        lets your samaritan take control of the terminal`);
+					this.echo(`desc <DID>			        returns DID document and metadata of samaritan`);
+					this.echo(`profile <keyN=valueN>       update the profile of your samaritan. Apps use this data by default e.g at signups. Absence of arguments displays data`);
+					this.echo(`help                        informs you about the samaritan terminal`);
+
+					break;
+
+				case "new":
+					// check argument conformance
+					if (!arg2) {
+						this.echo(`fatal: You must provide a name for your samaritan`);
+						this.echo(`usage: sam new <name>`);
+					} else {
+						this.echo(`creating your samaritan...`);
+						this.pause();
+
+						fetch(getURL(`new`, `name=${arg2}`), {
+							method: 'get',
+							headers: {
+								'Content-Type': 'application/json'
+							}
+						})
+							.then(async res => {
+								await res.json().then(res => {
+									main.resume();
+
+									if (res.error)
+										main.echo(`fatal: ${res.data.msg}`);
+									else {
+										// set nonce for further communication
+										sessionStorage.setItem("nonce", res.nonce);
+
+										main.echo("samaritan successfully added to the network");
+
+										main.echo(`DID:     ${res.data.did}`);
+										main.echo(`Keys:    ${res.data.keys} ([[b;red;] You have 30 seconds to copy them.])`);
+
+										main.pause();
+										setTimeout(() => {
+											main.update(-1, "Keys:    ****************************************************************************************************").resume();
+										}, 30000);
+									}
+
+								});
+							})
+					}
+
+					break;
+				case "init":
+					// check argument conformance
+					if (!arg2 || arguments.length > 2) {
+						this.echo(`fatal: you must provide your samaritan keys`);
+						this.echo(`usage: sam init <keys> e.g sam init "bake egypt below..."`);
+					} else {
+
+						// check length of mnemonic
+						if (arg2.split(/\s+/).length != 12) {
+							this.echo(`fatal: invalid number of mnemonic`);
+						} else {
+							// clear seeds
+							this.clear();
+							this.echo(`initializing samaritan...`);
+							this.pause();
+
+							fetch(getURL(`init`, `keys=${arg2}`), {
+								method: 'get',
+								headers: {
+									'Content-Type': 'application/json'
+								}
+							})
+								.then(async res => {
+									await res.json().then(res => {
+										main.resume();
+
+										if (res.error)
+											main.echo(`fatal: ${res.data.msg}`);
+										else {
+											// set nonce for further communication
+											sessionStorage.setItem("nonce", res.nonce);
+											main.echo(`${res.data.msg}`);
+										}
+									});
+								})
+						}
+					}
+
+					break;
+
+				case "desc":
+					if (!inSession()) {
+						main.echo(`fatal: no samaritan recognized. See 'sam help'`)
+					} else {
+						// check argument conformance
+						if (!arg2) {
+							this.echo(`fatal: you must provide DID to lookup`);
+							this.echo(`usage: sam find <DID>`);
+						} else {
+							// check did format
+							if (!isDID(arg2)) {
+								this.echo(`fatal: invalid DID format`);
+								this.echo(`expected DID format: did:sam:root:<address>`);
+							} else {
+								this.echo(`querying network...`);
+								this.pause();
+
+								fetch(getURL(`desc`, `did=${arg2}`), {
+									method: 'get',
+									headers: {
+										'Content-Type': 'application/json'
+									}
+								})
+									.then(res => {
+										(async function () {
+											await res.json().then(res => {
+												main.resume();
+
+												if (res.error)
+													main.echo(`fatal: ${res.data.msg}`);
+												else {
+													main.echo(`DID:     ${arg2}`);
+													main.echo(`Name:    ${res.data.name}`);
+												}
+											})
+										})();
+									})
+							}
+						}
+					}
+
+					break;
+
+				case "profile":
+					if (!inSession()) {
+						main.echo(`fatal: no samaritan initialized. See 'sam help'`);
+					} else {
+						if (!arg2)
+							this.echo(`fetching profile...`);
+						else
+							this.echo(`updating profile...`);
+						this.pause();
+
+						fetch(getURL(`profile`, `nonce=${getNonce()}`, `data=${arg2.replaceAll(" ", ";")}`), {
+							method: 'get',
+							headers: {
+								'Content-Type': 'application/json'
+							}
+						})
+							.then(res => {
+								(async function () {
+									await res.json().then(res => {
+										main.resume();
+
+										if (res.error)
+											main.echo(`fatal: ${res.data.msg}`);
+										else {
+											if (res.data.type == "set")
+												main.echo(`${res.data.msg}`);
+											else {
+												main.echo(`Profile data:`);
+												Object.entries(res.data.profile).map(([key, value]) => `${key}: ${value}`)
+											}
+										}
+									});
+								})();
+							})
+					}
+
+					break;
+
+
+				default:
+					this.echo(`sam: '${arg1}' is not a samaritan command. See 'sam help'.`);
+			}
+		},
+	}, {
+		greetings: function () {
+			return greetings.innerHTML
+		},
+
+		name: 'samaritan',
+		historySize: 10,
+		checkArity: false,
+		prompt: '[[b;green;]>>> ]',
+		onInit: function () {
+
+		}
+	});
+});
+
+function downloadObjectAsJson(exportObj, exportName) {
+	var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(exportObj);
+	var dn = document.createElement('a');
+
+	dn.setAttribute("href", dataStr);
+	dn.setAttribute("download", exportName + ".jsonld");
+	document.body.appendChild(dn); // required for firefox
+
+	dn.click();
+	dn.remove();
+}
+
+function qs(tag) {
+	return document.querySelector(tag);
+}
+
+function inSession() {
+	var exists = false;
+	if (sessionStorage.getItem("nonce"))
+		exists = true;
+
+	return exists;
+}
+
+function getNonce() {
+	return sessionStorage.getItem("nonce");
+}
+
+function isDID(str) {
+	if (str.indexOf("did:sam:root") == -1)
+		return false;
+
+	return true;
+}
+
+function isJSONLink(str) {
+	if (str.indexOf("http") == -1 || !str.endsWith(".json"))
+		return false;
+
+	return true;
+}
+
+function isGoodURL(url) {
+	let isErrorFree = true;
+
+	// break it up
+	let box = url.split("/");
+
+	// first, the url must start with a DID URI
+	if (!url.startsWith("did:sam:root:") || box.length != 4)
+		isErrorFree = false;
+
+	return isErrorFree;
+}
+
+function isGoodMode(mode) {
+	for (var i = 0; i < mode.length; i++)
+		if (parseInt(mode[i]) > 7 || parseInt(mode[i] < 0))
+			return false;
+
+	return true;
+}
+
+function getURL() {
+	let url = `\\${arguments[0]}?`;
+	for (var i = 1; i < arguments.length; i++)
+		url += `${arguments[i]}&`;
+
+	return url;
+}
+
+var printObj = function (obj) {
+	var string = '';
+
+	for (var prop in obj) {
+		if (typeof obj[prop] == 'string') {
+			string += prop + ': ' + obj[prop] + '; \n';
+		}
+		else {
+			string += prop + ': { \n' + print(obj[prop]) + '}';
+		}
+	}
+
+	return string;
+}
